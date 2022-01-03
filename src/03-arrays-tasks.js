f70f2f89747573426e6e14a9cafd6a099a418b1b
Double-Domination <squremeta@gmail.com>
2022-01-03 22:22:50 +0700

    feat: get second items task

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -287,8 +287,22 @@ function getSecondItems(arr) {
  *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  const tmp = arr.map((curItem, curIndex) => {
+    const result = [];
+    result.push(curItem);
+    // console.log(result)
+    let currentValueMultplexCount = curIndex;
+
+    while (currentValueMultplexCount !== 0) {
+      result.push(curItem);
+      currentValueMultplexCount -= 1;
+    }
+
+    return result;
+  });
+
+  return tmp.flat();
 }
 
 /**
