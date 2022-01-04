abc904f2d9c3526f53cb7c0cb49c9512f929e720
Double-Domination <squremeta@gmail.com>
2022-01-04 20:24:40 +0700

    feat: tasks until groupby

--- src/tmp2.js
+++ src/tmp2.js
@@ -1,9 +1,57 @@
-// const init = [1, 2, 3, 3, 2, 1];
+// const initDTO = [
+//   { country: 'Belarus', city: 'Brest' },
+//   { country: 'Russia', city: 'Omsk' },
+//   { country: 'Russia', city: 'Samara' },
+//   { country: 'Belarus', city: 'Grodno' },
+//   { country: 'Belarus', city: 'Minsk' },
+//   { country: 'Poland', city: 'Lodz' },
+// ];
 
-// function distinct(arr) {
-//   let result = new Set(arr);
+// function group(array, keySelectorCallback, valueSelectorCallback) {
+//   let selectorProcessed = [];
+//   selectorProcessed = array.map(keySelectorCallback);
+//   selectorProcessed = new Set(selectorProcessed);
 
-//   return Array.from(result);
+//   const groups = {};
+//   Array.from(selectorProcessed).map((current) => {
+//     // eslint-disable-next-line no-return-assign
+//     return (groups[current] = [array.filter((inner) => {
+//       if(inner.current === current)
+//     })]);
+//   });
+
+//   let value
+
+//   return groups;
 // }
 
-// console.log(distinct(init));
+// console.log(
+//   group(
+//     initDTO,
+//     (item) => item.country,
+//     (item) => item.city
+//   )
+// );
+
+// const testObj = {
+//   first: 1,
+//   second: 2,
+// };
+
+// function group(initArr, criteria) {
+//   return initArr.reduce(
+//     (accum, current) => {
+//       const key =
+//         typeof criteria === 'function' ? criteria(current) : current[criteria];
+
+//       // eslint-disable-next-line no-prototype-builtins
+//       if () {
+//         // eslint-disable-next-line no-param-reassign
+//         accum[key] = [];
+//       }
+//     },
+//     { key: 'zero' }
+//   );
+// }
+
+// console.log(group(initDTO));
