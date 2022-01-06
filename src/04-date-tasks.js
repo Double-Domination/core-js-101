62459239edb5c18bba7f2ea4cdcff239cb717285
Double-Domination <squremeta@gmail.com>
2022-01-07 03:18:19 +0700

    feat:     timeSpanToString task

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -51,8 +51,8 @@ function parseDataFromIso8601(value) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  return new Date(date.getFullYear(), 1, 29).getMonth() === 1;
 }
 
 /**
@@ -70,8 +70,15 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(endDate, startDate) {
+  const d = new Date(startDate - endDate);
+  return `${d.getUTCHours().toString().padStart(2, '0')}:${d
+    .getMinutes()
+    .toString()
+    .padStart(2, '0')}:${d.getSeconds().toString().padStart(2, '0')}.${d
+    .getMilliseconds()
+    .toString()
+    .padStart(3, '0')}`;
 }
 
 /**
